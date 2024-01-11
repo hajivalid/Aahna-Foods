685ed616d11a01e9bd8251e0d6ca948c86c3034c
hajivalid <hdudekula@gemini-us.com>
2024-01-11 23:44:00 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello World from React!"),
+    React.createElement("h1", {}, "React!"),
     React.createElement("h2", {}, "Hello World from React!"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "Hello World from React!"),
-    React.createElement("h2", {}, "Hello World from React!"),
+    React.createElement("h2", {}, "React!"),
   ]),
 ]);
 
